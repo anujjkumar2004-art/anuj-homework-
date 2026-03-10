899894d2a7a958347685a0c7a6cd04fdace7a4c9
anujjkumar2004-art <anujjkumar.2004@gmail.com>
2026-03-10 14:32:17 +0530

    today hw

---
+++ Day 60 of hw/Employee.java
@@ -0,0 +1,24 @@
+
+package employee;
+import java.util.Scanner;
+class Employee 
+{
+    int id;
+    String name;
+    String department;
+    
+    Employee(int id, String name, String department) 
+    {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    void display() 
+    {
+        System.out.println("Employee ID : " + id);
+        System.out.println("Name        : " + name);
+        System.out.println("Department  : " + department);
+        System.out.println("---------------------------");
+    }
+}
